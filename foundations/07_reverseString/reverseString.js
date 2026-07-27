f0ae0b7c3a821f38126387761042343fb0e22861
NJ <niljohna77@gmail.com>
2026-07-26 21:02:38 -0700

    Finish 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringCopy = string.split('');
+    let stringNewArray = stringCopy.reverse();
+    return finalString = stringNewArray.join('');
 };
 
 // Do not edit below this line
