1938ba69557f376feeddc1fba1d88ebbb6157d7c
NJ <niljohna77@gmail.com>
2026-07-26 20:32:27 -0700

    Finish 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, iterTime) {
+    const stringCopy = string.slice(0);
+    let finalString = "";
+    if (iterTime < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < iterTime; i++) {
+        finalString = finalString.concat(stringCopy);
+    }
+    return finalString;
 };
 
 // Do not edit below this line
