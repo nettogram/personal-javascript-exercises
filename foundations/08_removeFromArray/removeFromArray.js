328af5e35e4aa929120fca8a07a61b8557aa99a6
NJ <niljohna77@gmail.com>
2026-07-28 11:28:03 -0700

    Finish 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...item) {
+    for (const value of item) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === value) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
