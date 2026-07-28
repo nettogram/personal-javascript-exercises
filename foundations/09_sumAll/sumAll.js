595da5d4888758ef1b30f3c58d07222e64a231e0
NJ <niljohna77@gmail.com>
2026-07-28 12:01:38 -0700

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const findBiggerNum = () => {
+        if (num1 < num2) {
+            return num2;
+        } else {
+            return num1;
+        }
+    }
+    let findFinalNum = () => {
+        if (num1 < num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
 
+    let totalSum = 0;
+    let biggerNum = findBiggerNum();
+    let finalNum = findFinalNum();
+
+    if ((Number.isInteger(num1) && Number.isInteger(num2)) &&
+        num1 > 0 && 
+        num2 > 0) { 
+        while (finalNum < biggerNum + 1) {
+            totalSum += finalNum;
+            finalNum++;
+        }
+    } else return "ERROR";
+
+    return totalSum;
 };
 
 // Do not edit below this line
