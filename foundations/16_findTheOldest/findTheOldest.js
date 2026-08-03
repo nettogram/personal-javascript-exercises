25efd610b4eb914f6411990472226df8ecfc9327
NJ <niljohna77@gmail.com>
2026-08-02 22:28:27 -0700

    Finish 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const arragedByAge = people.sort((p1, p2) => {
+        const currentYear = new Date().getFullYear();
+        const person1 = (p1.yearOfDeath ?? currentYear) - p1.yearOfBirth;
+        const person2 = (p2.yearOfDeath ?? currentYear) - p2.yearOfBirth;
+        return person1 > person2 ? -1 : 1;
+    });
+    return arragedByAge[0];
 };
 
 // Do not edit below this line
