c2a3163ade6d9c13d67926ba8678d4634fbd544d
NJ <niljohna77@gmail.com>
2026-08-02 18:47:50 -0700

    Finish 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+  return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNum) {
+  let totalSum = arrayOfNum.reduce((sum, current) => sum + current, 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNum) {
+  let total = arrayOfNum.reduce((product, current) => product * current);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let newArray = [];
+  const testForZero = number
+  while (number != 0) {
+    newArray.push(number);
+    number--;
+  }
+  if (testForZero == 0) {
+    return 1;
+  }
+  let factorialAns = newArray.reduce((product, current) => product * current);
+  return factorialAns;
 };
 
 // Do not edit below this line
