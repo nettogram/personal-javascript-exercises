226f55ef022e65359802766504de1598de216304
NJ <niljohna77@gmail.com>
2026-08-02 21:52:41 -0700

    Finish 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    const error = "OOPS";
+    let fibonacciSequence = [1, 1];
+    if (typeof(index) == "string") {
+        index = Number(index);
+    }
+    if (index == 1 || index == 2) {
+        return 1;
+    } else if (index == 0) {
+        return 0;
+    } else if (index < 0) {
+        return error;
+    } else {
+        for (let i = 2; i <= index; i++) {
+            fibonacciSequence.push(
+                                    fibonacciSequence[i - 2] + 
+                                    fibonacciSequence[i - 1]);
+        }
+        return fibonacciSequence[fibonacciSequence.length - 2];
+    }
 
 };
 
