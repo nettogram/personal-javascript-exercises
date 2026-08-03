e90c23babc10a6bc4b6f7f07401fcdef605cb367
NJ <niljohna77@gmail.com>
2026-08-02 21:02:24 -0700

    Finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let Container1 = Array.from(string);
+    Container1 = Container1.filter(char => (char != " " && 
+                                            char != "," && 
+                                            char != "!" && 
+                                            char != "."))
+    let left = 0;
+    let right = Container1.length - 1;
+    while (left < right) {
+        if (Container1[left] !== Container1[right]) {
+            return false
+        }
+        left++;
+        right--;
+    }
+    return true
 };
 
 // Do not edit below this line
